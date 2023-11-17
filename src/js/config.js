40fe6f4a53e78175b4586708db1ada4684f10207
elizaturnercodes <lizzylynnturner@gmail.com>
2023-11-17 17:10:01 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd44ae358-1074-41e0-8da9-c67d325231c4';
+export const MODAL_CLOSE_SEC = 2.5;
